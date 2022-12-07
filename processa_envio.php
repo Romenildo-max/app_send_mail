5aa532bb8eab17d738c2db60b2debb931664e2dd
Romenildo Silva <romenildo2017@outlook.com>
2022-12-06 21:33:04 -0300

    tualização

---
+++ processa_envio.php
@@ -0,0 +1,10 @@
+<?php
+
+    require "../app_send_mail_arquivo_protegido/bibliotecas/PHPMailer/Exception.php"; 
+    require "../app_send_mail_arquivo_protegido/bibliotecas/PHPMailer/OAuth.php";
+    require "../app_send_mail_arquivo_protegido/bibliotecas/PHPMailer/PHPMailer.php"; 
+    require "../app_send_mail_arquivo_protegido/bibliotecas/PHPMailer/POP3.php"; 
+    require "../app_send_mail_arquivo_protegido/bibliotecas/PHPMailer/SMTP.php"; 
+
+    require "../app_send_mail_arquivo_protegido/processa_envio.php";
+?>
